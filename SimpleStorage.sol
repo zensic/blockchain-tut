5e09d47f7c50a7259a9ac17f11d81fad812f13e8
Anthony Lee <anthonyleehj@gmail.com>
2022-02-11 16:28:34 +0800

    Finished Lesson 1: Simple storage with Remix

---
+++ SimpleStorage.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.6.0;
+
+//SPDX-License-Identifier: MIT
+
+contract SimpleStorage {
+
+    uint256 favouriteNumber;
+    bool favouriteBool;
+
+    struct People {
+        uint256 favouriteNumber;
+        string name;
+    }
+
+    People[] public people;
+    mapping(string => uint256) public nameToFavouriteNumber;
+
+    function store(uint256 _favouriteNumber) public {
+        favouriteNumber = _favouriteNumber;
+    }
+
+    function retrieve() public view returns(uint256) {
+        return favouriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favouriteNumber) public {
+        people.push(People(_favouriteNumber, _name));
+        nameToFavouriteNumber[_name] = _favouriteNumber;
+    }
+}
